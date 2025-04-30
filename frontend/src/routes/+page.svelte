8f6f8cc3be3a01c8bf3830c0482f9c0b74f9810c
rena1208 <jajajajam07@gmail.com>
2025-04-30 14:06:59 +0900

    FastAPIとSveltekitのセットアップ

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	async function fetchData() {
+		console.log('おした');
+		// const url = 'http://127.0.0.1:8000/api'; // FastAPI エンドポイント
+		try {
+			const response = await fetch(`/api`);
+			if (response.ok) {
+				console.log(`/api`);
+				// throw new Error(`HTTP error! status: ${response.status}`);
+				const data = await response.json();
+				console.log(data);
+			}
+		} catch (e) {
+			console.error('Failed to fetch data:', e);
+		}
+	}
+</script>
+<button on:click={fetchData}>Fetch Data</button>
